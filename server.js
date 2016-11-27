c7033ecb475850ebdf7498937a5e5c29471685c4
KBSimpson <keller.simpson94@gmail.com>
2016-11-27 14:18:35 -0500

    Adjusted the \\ to /

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var app=express();
 var port=process.env.PORT || 8080; 
 app.get('/', function(req, res){
-	res.sendFile(__dirname+'\\SecSanta.html');
+	res.sendFile(__dirname+'/SecSanta.html');
 });
 app.listen(port, function(){
 	console.log("I'm listening on port: "+port);
